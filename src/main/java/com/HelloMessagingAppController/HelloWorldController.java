47aa9627d11f9d7f6be041a76f34ba5f10f61468
mohdarshad1 <mdarshad181@gmail.com>
2021-06-15 21:49:19 +0530

    [Mohd Arshad] : REST Call Path Variable

--- src/main/java/com/HelloMessagingAppController/HelloWorldController.java
+++ src/main/java/com/HelloMessagingAppController/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.HelloMessagingAppController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -15,9 +12,17 @@ public class HelloWorldController {
         return "Hello from Bridgelabz";
     }
 
+    // Query Parameter
     // GET Request:- Rest call: curl localhost:8080/hello/query?name=Mark -w "\n"
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String hello(@RequestParam(value = "name") String name){
         return "Hello " + name + " !!";
+    }
+
+ // Path Variable
+    // GET Request:- Rest call: curl localhost:8080/hello/param/Mark -w "\n"
+    @GetMapping("/param/{name}")
+    public String helloParam(@PathVariable String name){
+        return "Hello " + name +" !!";
     }
 }

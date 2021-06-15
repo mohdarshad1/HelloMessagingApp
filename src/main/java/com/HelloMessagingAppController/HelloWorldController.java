895e902c868a8b4821d481d133334c26e44954d7
mohdarshad1 <mdarshad181@gmail.com>
2021-06-15 21:59:58 +0530

    [Mohd Arshad] : PUT Request Method First Name as Path Variable and last name as Query Parameter

--- src/main/java/com/HelloMessagingAppController/HelloWorldController.java
+++ src/main/java/com/HelloMessagingAppController/HelloWorldController.java
@@ -9,28 +9,34 @@ public class HelloWorldController {
 
     // GET Request :- Rest call: curl localhost:8080/hello -w "\n"
     @RequestMapping(value = {"", "/", "/home"})
-    public String hello(){
+    public String hello() {
         return "Hello from Bridgelabz";
     }
 
     // Query Parameter
     // GET Request:- Rest call: curl localhost:8080/hello/query?name=Mark -w "\n"
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
-    public String hello(@RequestParam(value = "name") String name){
+    public String hello(@RequestParam(value = "name") String name) {
         return "Hello " + name + " !!";
     }
 
     // Path Variable
     // GET Request:- Rest call: curl localhost:8080/hello/param/Mark -w "\n"
     @GetMapping("/param/{name}")
-    public String helloParam(@PathVariable String name){
-        return "Hello " + name +" !!";
+    public String helloParam(@PathVariable String name) {
+        return "Hello " + name + " !!";
     }
 
- // Post Request:- Rest call: curl -X POST -H "Content-Type: application/json" -d '{\"firstName\": \”Mark\", \"lastName\": \”Taylor\"}'
-    //                           "http://localhost:8080/hello/post" -w "\n"
+    // Post Request:- Rest call: curl -H "Content-Type: application/json" -X POST http://localhost:8080/hello/post -d "{\
+    // "firstName\":\"Mark\", \"lastName\":\"Taylor\"}"
     @PostMapping("/post")
-    public String hello(@RequestBody User user){
+    public String hello(@RequestBody User user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " !!";
+    }
+
+    // Put Request:- Rest call: curl -H "Content-Type: application/json" -X PUT http://localhost:8080/hello/put/Abhishek -d "{\"lastName\":\"Das\"}"
+    @PutMapping("/put/{firstName}")
+    public String hello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+        return "Hello " + firstName + " " + lastName + " !!";
     }
 }
